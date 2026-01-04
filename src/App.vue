00673a8195717e165a50a8d2854a73fb110805e1
Shiro Wang <startraceebr@foxmail.com>
2026-01-04 17:56:55 +0800

    format styles

--- src/App.vue
+++ src/App.vue
@@ -29,9 +29,11 @@ export default {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
